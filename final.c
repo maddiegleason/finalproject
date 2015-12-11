c6673e409471a1d0869f96bb46ead0ce9046406b
trongaus <trongaus@aol.com>
2015-12-11 16:10:05 -0500

    post-library Friday afternoon edition

--- final.c
+++ final.c
@@ -28,11 +28,11 @@
 
 void drawBackground();
 void drawLilyPads();
-void drawTruck(int *dx);
-void drawCar(int *dx);
-void drawLog(int *dx);
+void drawTruck(int *dxtruck1, int *dxtruck2);
+void drawCar(int *dxcar1, int *dxcar2);
+void drawLog(int *dxlog1, int *dxlog2, int *dxlog3);
 void drawFrog(int *dxfrog, int *dyfrog);
-void drawTurtle(int *dx);
+void drawTurtle(int *dxturtle);
 
 int main()
 {
@@ -40,7 +40,15 @@ int main()
 	int dx = 0;					// initialize background movement in the x direction
 	int dxfrog = 0;				// initialize frog hop coordinates
 	int dyfrog = 0;
-	int deltat = 30000;
+	int deltat = 25000;
+	int dxtruck1 = 0;
+	int dxtruck2 = 0;
+	int dxcar1 = 0;
+	int dxcar2 = 0;
+	int dxlog1 = 0;
+	int dxlog2 = 0;
+	int dxlog3 = 0;
+	int dxturtle = 0;
 
 	// Open Window
 	gfx_open(XSIZE, YSIZE, "Frogger");
@@ -50,44 +58,43 @@ int main()
 	if (dx > XSIZE) {
 		dx = 0;
 	}
-		
-		switch(c) {
-			case 'R': // up arrow
-				if (dyfrog > -12) {
-					dyfrog--;
-				}
-				drawFrog(&dxfrog,&dyfrog);
-				c = '+';
-				break;
-			case 'Q': // left arrow
-				if (dxfrog > -7) {
-					dxfrog--;
-				}
-				drawFrog(&dxfrog,&dyfrog);
-				c = '+';
-				break;
-			case 'T': // down arrow
-				if (dyfrog < 0) {
-					dyfrog++;
-				}
-				drawFrog(&dxfrog,&dyfrog);
-				c = '+';
-				break;
-			case 'S': // right arrow
-				if (dxfrog<5) {
-					dxfrog++;
-				}
-				drawFrog(&dxfrog,&dyfrog);
-				c = '+';
-				break;
+	switch(c) {
+		case 'R': // up arrow
+			if (dyfrog > -12) {
+				dyfrog--;
+			}
+			drawFrog(&dxfrog,&dyfrog);
+			c = '+';
+			break;
+		case 'Q': // left arrow
+			if (dxfrog > -7) {
+				dxfrog--;
+			}
+			drawFrog(&dxfrog,&dyfrog);
+			c = '+';
+			break;
+		case 'T': // down arrow
+			if (dyfrog < 0) {
+				dyfrog++;
+			}
+			drawFrog(&dxfrog,&dyfrog);
+			c = '+';
+			break;
+		case 'S': // right arrow
+			if (dxfrog<5) {
+				dxfrog++;
+			}
+			drawFrog(&dxfrog,&dyfrog);
+			c = '+';
+			break;
 		}
 		
 		gfx_clear();
 		drawBackground();
 		drawLilyPads();
-		drawTruck(&dx);
-		drawCar(&dx);
-		drawLog(&dx);
+		drawTruck(&dxtruck1, &dxtruck2);
+		drawCar(&dxcar1,&dxcar2);
+		drawLog(&dxlog1,&dxlog2,&dxlog3);
 		drawTurtle(&dx);
 		drawFrog(&dxfrog,&dyfrog);
 
@@ -138,153 +145,134 @@ void drawBackground()
 
 void drawLilyPads()
 {
-		gfx_color(243, 164, 246);
-		double k;
-		for (k = 2.5; k <=10.5; k = k+2){
-			gfx_fill_arc((k*XSCALE-20),(.5*YSCALE-20),40,40,0,360);
-		}
+	gfx_color(243, 164, 246);
+	double k;
+	for (k = 2.5; k <=10.5; k = k+2){
+		gfx_fill_arc((k*XSCALE-20),(.5*YSCALE-20),40,40,0,360);
+	}
 }
 
 // NOTE: for moving objects truck, car, and log only need to increment x position
 // Cannot figure out how to generate multiple objects in one line going across
 // tried for loop incrementing i in the x position and that just changed speed? 
 
-void drawTruck(int *dx)
+void drawTruck(int *dxtruck1, int *dxtruck2) 
 {
-	double i, j;
-	for (j=-1; j <= 1 ; j+=0.5){	
-		for (i=0; i<=13; i+=13) {
-		
-			// Truck Body 
-			gfx_color(255,255,255);
-			gfx_fill_rectangle(0+*dx+i*j*XSCALE,YSIZE-(2.8*YSCALE),(1.5*XSCALE),(.6*YSCALE));
-			// Truck top 
-			gfx_color(210, 27, 27);
-			gfx_fill_rectangle((1.5*XSCALE+i*j*XSCALE) + *dx,YSIZE-(2.8*YSCALE),(.5*XSCALE),(.6*YSCALE));
-			// Truck Middle 
-			gfx_color(0, 0, 0);
-			gfx_fill_rectangle((1.5*XSCALE+i*j*XSCALE) + *dx,YSIZE-(2.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
-			// Truck Window
-			gfx_color(81, 227, 255);
-			gfx_fill_rectangle((1.85*XSCALE+i*j*XSCALE) + *dx, YSIZE-(2.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
+	double i, j, k;
+	for (k=0; k<=5;k+=4) {
+		for (j=-1; j <= 1 ; j+=0.5){	
+			for (i=0; i<=130; i+=13) {
+				// TRUCKS GOING RIGHT
+				// Truck Body 
+				gfx_color(255,255,255);
+				gfx_fill_rectangle((k+0)*XSCALE+i*j*XSCALE + *dxtruck1,YSIZE-((k+2.8)*YSCALE),(1.5*XSCALE),(.6*YSCALE));
+				// Truck top 
+				gfx_color(210, 27, 27);
+				gfx_fill_rectangle(((k+1.5)*XSCALE+i*j*XSCALE) + *dxtruck1,YSIZE-((k+2.8)*YSCALE),(.5*XSCALE),(.6*YSCALE));
+				// Truck Middle 
+				gfx_color(0, 0, 0);
+				gfx_fill_rectangle(((k+1.5)*XSCALE+i*j*XSCALE) + *dxtruck1,YSIZE-((k+2.8)*YSCALE),(.15*XSCALE),(.6*YSCALE));
+				// Truck Window
+				gfx_color(81, 227, 255);
+				gfx_fill_rectangle(((k+1.85)*XSCALE+i*j*XSCALE) + *dxtruck1, YSIZE-((k+2.8)*YSCALE),(.15*XSCALE),(.6*YSCALE));
+
+				// TRUCKs GOING LEFT
+				// Truck Body 
+				gfx_color(255,255,255);
+				gfx_fill_rectangle(XSIZE-(1.5*XSCALE+i*j*XSCALE) - *dxtruck2,YSIZE-(4.8*YSCALE),(1.5*XSCALE),(.6*YSCALE));
+				// Truck top 
+				gfx_color(210, 27, 27);
+				gfx_fill_rectangle(XSIZE-(2.0*XSCALE+i*j*XSCALE) - *dxtruck2,YSIZE-(4.8*YSCALE),(.5*XSCALE),(.6*YSCALE));
+				// Truck Middle 
+				gfx_color(0, 0, 0);
+				gfx_fill_rectangle(XSIZE-(1.65*XSCALE+i*j*XSCALE) - *dxtruck2,YSIZE-(4.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
+				// Truck Window
+				gfx_color(81, 227, 255);
+				gfx_fill_rectangle(XSIZE-(2.00*XSCALE+i*j*XSCALE) - *dxtruck2, YSIZE-(4.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
+			}	
 		}	
-	}
+	}	
+	(*dxtruck1)++;
+	(*dxtruck2)+=2;
+		
+
+}
+
+void drawCar(int *dxcar1, int *dxcar2)
+{
+	double i,j;
 	for (j=-1; j <=1; j+=0.5){
-		for (i=0;i<=13;i+=13) {
-			// Truck Body 
-			gfx_color(255,255,255);
-			gfx_fill_rectangle(4*XSCALE+*dx+i*j*XSCALE,YSIZE-(6.8*YSCALE),(1.5*XSCALE),(.6*YSCALE));
-			// Truck top 
-			gfx_color(210, 27, 27);
-			gfx_fill_rectangle((5.5*XSCALE+i*j*XSCALE) + *dx,YSIZE-(6.8*YSCALE),(.5*XSCALE),(.6*YSCALE));
-			// Truck Middle 
-			gfx_color(0, 0, 0);
-			gfx_fill_rectangle((5.5*XSCALE+i*j*XSCALE) + *dx,YSIZE-(6.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
-			// Truck Window
+		for (i=0;i<=130;i+=13) {
+			// CAR GOING RIGHT
+			// Car Body
+			gfx_color(165, 29, 124);
+			gfx_fill_rectangle(((3*XSCALE) + *dxcar1 +i*j*XSCALE),YSIZE-(3.8*YSCALE),XSCALE,(.6*YSCALE));
+			// Headlights
+			gfx_color(253, 234, 59);
+			gfx_fill_rectangle(((3.9*XSCALE) + *dxcar1+i*j*XSCALE),YSIZE-(3.8*YSCALE),(.1*XSCALE),(.1*YSCALE));
+			gfx_fill_rectangle(((3.9*XSCALE) + *dxcar1+i*j*XSCALE),YSIZE-(3.3*YSCALE),(.1*XSCALE),(.1*YSCALE));
+			// Windows
 			gfx_color(81, 227, 255);
-			gfx_fill_rectangle((5.85*XSCALE+i*j*XSCALE) + *dx, YSIZE-(6.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
+			gfx_fill_rectangle(((3.3*XSCALE) + *dxcar1+i*j*XSCALE),YSIZE-(3.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
+			gfx_fill_rectangle(((3.6*XSCALE) + *dxcar1+i*j*XSCALE),YSIZE-(3.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
+
+			// CAR GOING LEFT
+			// Car Body
+			gfx_color(165, 29, 124);
+			gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dxcar2+i*j*XSCALE),YSIZE-(5.8*YSCALE),XSCALE,(.6*YSCALE));
+			// Headlights
+			gfx_color(253, 234, 59);
+			gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dxcar2+i*j*XSCALE),YSIZE-(5.8*YSCALE),(.1*XSCALE),(.1*YSCALE));
+			gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dxcar2+i*j*XSCALE),YSIZE-(5.3*YSCALE),(.1*XSCALE),(.1*YSCALE));
+			// Windows
+			gfx_color(81, 227, 255);
+			gfx_fill_rectangle((XSIZE-(3.4*XSCALE) - *dxcar2+i*j*XSCALE),YSIZE-(5.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
+			gfx_fill_rectangle((XSIZE-(3.7*XSCALE) - *dxcar2+i*j*XSCALE),YSIZE-(5.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
 		}
 	}
-	(*dx)++;
-
-	/* TRUCK GOING LEFT
-	// Truck Body 
-	gfx_color(255,255,255);
-	gfx_fill_rectangle(XSIZE-(1.5*XSCALE) - *dx,YSIZE-(4.8*YSCALE),(1.5*XSCALE),(.6*YSCALE));
-	// Truck top 
-	gfx_color(210, 27, 27);
-	gfx_fill_rectangle(XSIZE-(2.0*XSCALE) - *dx,YSIZE-(4.8*YSCALE),(.5*XSCALE),(.6*YSCALE));
-	// Truck Middle 
-	gfx_color(0, 0, 0);
-	gfx_fill_rectangle(XSIZE-(1.65*XSCALE) - *dx,YSIZE-(4.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
-	// Truck Window
-	gfx_color(81, 227, 255);
-	gfx_fill_rectangle(XSIZE-(2.00*XSCALE) - *dx, YSIZE-(4.8*YSCALE),(.15*XSCALE),(.6*YSCALE));
-	(*dx)--;*/
-
+	
+	(*dxcar1)++;
+	(*dxcar2)+=2;
 }
 
-void drawCar(int *dx)
+void drawLog(int *dxlog1, int *dxlog2, int *dxlog3)
 {
-	/* CAR GOING RIGHT
-	// Car Body
-	gfx_color(165, 29, 124);
-	gfx_fill_rectangle(((1*XSCALE) + *dx),YSIZE-(3.8*YSCALE),XSCALE,(.6*YSCALE));
-	// Headlights
-	gfx_color(253, 234, 59);
-	gfx_fill_rectangle(((1.9*XSCALE) + *dx),YSIZE-(3.8*YSCALE),(.1*XSCALE),(.1*YSCALE));
-	gfx_fill_rectangle(((1.9*XSCALE) + *dx),YSIZE-(3.3*YSCALE),(.1*XSCALE),(.1*YSCALE));
-	// Windows
-	gfx_color(81, 227, 255);
-	gfx_fill_rectangle(((1.3*XSCALE) + *dx),YSIZE-(3.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
-	gfx_fill_rectangle(((1.6*XSCALE) + *dx),YSIZE-(3.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
-	(*dx)++;
-
-	// CAR GOING LEFT
-	// Car Body
-	gfx_color(165, 29, 124);
-	gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dx),YSIZE-(5.8*YSCALE),XSCALE,(.6*YSCALE));
-	// Headlights
-	gfx_color(253, 234, 59);
-	gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dx),YSIZE-(5.8*YSCALE),(.1*XSCALE),(.1*YSCALE));
-	gfx_fill_rectangle((XSIZE-(4*XSCALE) - *dx),YSIZE-(5.3*YSCALE),(.1*XSCALE),(.1*YSCALE));
-	// Windows
-	gfx_color(81, 227, 255);
-	gfx_fill_rectangle((XSIZE-(3.4*XSCALE) - *dx),YSIZE-(5.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
-	gfx_fill_rectangle((XSIZE-(3.7*XSCALE) - *dx),YSIZE-(5.65*YSCALE),(.1*XSCALE),(.3*YSCALE));
-	(*dx)--;
-*/
+	double i,j;
+	for (j=-5; j <=5; j+=0.5){
+		for (i=0;i<=130;i+=13) {
+			gfx_color(121, 68, 7);
+			gfx_fill_rectangle((7*XSCALE)+*dxlog1+i*j*XSCALE,YSIZE-(9.8*YSCALE),(2.5*XSCALE),(.6*YSCALE));
+			gfx_fill_rectangle((3*XSCALE)-*dxlog2+i*j*XSCALE,YSIZE-(10.8*YSCALE),(2*XSCALE),(.6*YSCALE));
+			gfx_fill_rectangle((9*XSCALE)+*dxlog3+i*j*XSCALE,YSIZE-(11.8*YSCALE),(3*XSCALE),(.6*YSCALE));
+		}
+	}
+	(*dxlog1)+=2;
+	(*dxlog2)++;
+	(*dxlog3)+=3;
 }
 
-void drawLog(int *dx)
+void drawTurtle(int *dxturtle)
 {
-	/*gfx_color(121, 68, 7);
-	gfx_fill_rectangle((7*XSCALE)+*dx,YSIZE-(9.8*YSCALE),(2.5*XSCALE),(.6*YSCALE));
-	gfx_fill_rectangle((3*XSCALE)+*dx,YSIZE-(10.8*YSCALE),(2*XSCALE),(.6*YSCALE));
-	gfx_fill_rectangle((9*XSCALE)+*dx,YSIZE-(11.8*YSCALE),(3*XSCALE),(.6*YSCALE));
-	
-	(*dx)++;*/
+/*	double i,j,k;
+	for (j=-5; j <=5; j+=0.5){
+		for (i=0;i<=130;i+=13) {
+			for (k=3;k<=5;i++) {
+				gfx_color(9, 110, 23);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE) + *dxturtle+i*j*XSCALE), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 0, 180);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE) + *dxturtle+i*j*XSCALE), (YSIZE-(9.7*YSCALE)), (.7*YSCALE),(.5*YSCALE), 180,360);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE)+ 27 + *dxturtle+i*j*XSCALE), (YSIZE-9.45*YSCALE)-7.5, 15, 15, 0, 360);
+				gfx_color(255, 255, 255);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE)+ 37 + *dxturtle+i*j*XSCALE), (YSIZE-9.25*YSCALE)-7.5, 5, 5, 0, 360);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE)+ 37 + *dxturtle+i*j*XSCALE), (YSIZE-9.45*YSCALE)-7.5, 5, 5, 0, 360);
+				gfx_color(0, 0, 0);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE)+ 39 + *dxturtle+i*j*XSCALE), (YSIZE-9.25*YSCALE)-7.5, 4, 4, 0, 360);
+				gfx_fill_arc((k*XSCALE-(.25*XSCALE)+ 39 + *dxturtle+i*j*XSCALE), (YSIZE-9.45*YSCALE)-7.5, 4, 4, 0, 360);
+		}
+	}
 }
-
-void drawTurtle(int *dx)
-{
-	/* HOW CAN WE MAKE THREE TURTLES WITH A FOR LOOP? 
-	// Turtle 1
-	gfx_color(9, 110, 23);
-	gfx_fill_arc((5*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 0, 180);
-	gfx_fill_arc((5*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 180, 360);
-	gfx_fill_arc((5*XSCALE-(.25*XSCALE)+ 27 + *dx), (YSIZE-9.45*YSCALE)-7.5, 15, 15, 0, 360);
-	gfx_color(255, 255, 255);
-	gfx_fill_arc((5*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.25*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_fill_arc((5*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.45*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_color(0, 0, 0);
-	gfx_fill_arc((5*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.25*YSCALE)-7.5, 4, 4, 0, 360);
-	gfx_fill_arc((5*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.45*YSCALE)-7.5, 4, 4, 0, 360);
 	
-	// Turtle 2
-	gfx_color(9, 110, 23);
-	gfx_fill_arc((4*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 0, 180);
-	gfx_fill_arc((4*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 180, 360);
-	gfx_fill_arc((4*XSCALE-(.25*XSCALE)+ 27 + *dx), (YSIZE-9.45*YSCALE)-7.5, 15, 15, 0, 360);
-	gfx_color(255, 255, 255);
-	gfx_fill_arc((4*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.25*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_fill_arc((4*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.45*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_color(0, 0, 0);
-	gfx_fill_arc((4*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.25*YSCALE)-7.5, 4, 4, 0, 360);
-	gfx_fill_arc((4*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.45*YSCALE)-7.5, 4, 4, 0, 360);
-	
-	// Turtle 3	
-	gfx_color(9, 110, 23);
-	gfx_fill_arc((3*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 0, 180);
-	gfx_fill_arc((3*XSCALE - (.25*XSCALE) + *dx), (YSIZE-(9.7*YSCALE)), (.7*YSCALE), (.5*YSCALE), 180, 360);
-	gfx_fill_arc((3*XSCALE-(.25*XSCALE)+ 27 + *dx), (YSIZE-9.45*YSCALE)-7.5, 15, 15, 0, 360);
-	gfx_color(255, 255, 255);
-	gfx_fill_arc((3*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.25*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_fill_arc((3*XSCALE-(.25*XSCALE)+ 37 + *dx), (YSIZE-9.45*YSCALE)-7.5, 5, 5, 0, 360);
-	gfx_color(0, 0, 0);
-	gfx_fill_arc((3*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.25*YSCALE)-7.5, 4, 4, 0, 360);
-	gfx_fill_arc((3*XSCALE-(.25*XSCALE)+ 39 + *dx), (YSIZE-9.45*YSCALE)-7.5, 4, 4, 0, 360);
-	*/
+	(*dxturtle)+=2;
+*/	
 }
 
 void drawFrog(int *dxfrog, int *dyfrog)
